0f40413c8cc2a5d80b5bc28ad24dd4b1d8e4c661
NessIsHappy <vercettiswims@gmail.com>
2024-06-30 16:55:57 +0300

    Genre button count

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 document.getElementById("clickButton").addEventListener("click", function() {
     window.location.href = "FilmsChooser/index.html"; // Замените ссылку на нужную вам
 });
+
+let selectedCount = 0;
+
+document.querySelectorAll('.genreButton').forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.classList.contains('selected')) {
+            button.classList.remove('selected');
+            selectedCount--;
+        } else if (selectedCount < 4) {
+            button.classList.add('selected');
+            selectedCount++;
+        }
+
+        document.getElementById('genreCount').innerText = selectedCount;
+    });
+});
+
