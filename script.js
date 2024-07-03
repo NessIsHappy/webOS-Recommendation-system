2a8a4e9852408ed774e9690fe42b1d64cf4e9439
NessIsHappy <vercettiswims@gmail.com>
2024-07-03 13:43:11 +0300

    Correct pics for genres

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 let selectedCount = 0;
+let selectedGenres = [];
 
 document.querySelectorAll('.genreButton').forEach(button => {
     button.addEventListener('click', () => {
         if (button.classList.contains('selected')) {
             button.classList.remove('selected');
+            selectedGenres = selectedGenres.filter(genre => genre !== button.textContent);
             selectedCount--;
         } else if (selectedCount < 4) {
             button.classList.add('selected');
+            selectedGenres.push(button.textContent);
             selectedCount++;
         }
-
+        document.getElementById('selectedGenres').innerText = selectedGenres;
         document.getElementById('genreCount').innerText = selectedCount;
     });
 });
 
 document.getElementById("clickButton").addEventListener("click", function() {
     if (selectedCount == 4) {
-        window.location.href = "FilmsChooser/index.html"; // Замените ссылку на нужную вам
+        let selectedGenresStr = selectedGenres.join(',');
+        window.location.href = "FilmsChooser/index.html?genres=" + encodeURIComponent(selectedGenresStr);
     } else {
         let header = document.getElementById("header");
         header.innerText = `You chose only ${selectedCount} genre(s)!`;
--- test.js
+++ test.js
@@ -1,50 +1,77 @@
 const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYjAzZjBjNjIwYTY0MTM0NjU3MzJmYzE3ZTIxZmI0NCIsIm5iZiI6MTcxOTc0NzY3NS44OTMzODgsInN1YiI6IjY2ODE0MzYxY2JkMzE2ZDRiYWFjMzRjMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6McmaAHqRsfHN6Vi4ddza0SMzui-88zaK2_dqdj13LM'
-    }
-  };
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYjAzZjBjNjIwYTY0MTM0NjU3MzJmYzE3ZTIxZmI0NCIsIm5iZiI6MTcxOTc0NzY3NS44OTMzODgsInN1YiI6IjY2ODE0MzYxY2JkMzE2ZDRiYWFjMzRjMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6McmaAHqRsfHN6Vi4ddza0SMzui-88zaK2_dqdj13LM'
+  }
+};
+
+let genres = {
+  'Action': 28,
+  'Adventure': 12,
+  'Animation': 16,
+  'Comedy': 35,
+  'Crime': 80,
+  'Documentary': 99,
+  'Drama': 18,
+  'Fantasy': 14,
+  'History': 36,
+  'Horror': 27,
+  'Sci-Fi': 878,
+  'Thriller': 53,
+};
 
 let api_string = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=';
 let desired_genre = [28, 12, 16, 35];
 let array_api = [api_string, api_string, api_string, api_string];
 
+let urlParams = new URLSearchParams(window.location.search);
+let selectedGenres = urlParams.get('genres').split(',');
+
 for (let i = 0; i < 4; i++) {
-    array_api[i] += desired_genre[i];
+  desired_genre[i] = genres[selectedGenres[i]];
+}
+
+console.log(desired_genre);
+console.log(genres['Action']);
+console.log(selectedGenres);
+console.log(genres[selectedGenres[0]]);
+
+for (let i = 0; i < 4; i++) {
+  array_api[i] += desired_genre[i];
 }
 
 let savedData;
 
 async function fetchData(i) {
-  try {
-    const response = await fetch(array_api[i], options);
-    const data = await response.json();
+try {
+  const response = await fetch(array_api[i], options);
+  const data = await response.json();
 
-    savedData = data;
+  savedData = data;
 
-  } catch (error) {
-    console.error('Ошибка:', error);
-  }
+} catch (error) {
+  console.error('Ошибка:', error);
+}
 }
 
 let images = [];
 
 for (let i = 0; i < 4; i++) {
-    fetchData(i).then(() => {
-        let cnt = savedData["results"].length;
-        for (let j = 0; j < cnt; j++) {
-            //console.log(savedData["results"][j]);
-            images[i * 5 + j] = ("http://image.tmdb.org/t/p/w500" + savedData["results"][j]["backdrop_path"]);
-            console.log(images[i]);
-        }
-        console.log(i, "___________");
-        let genreDivs = document.querySelectorAll('.genre');
+  fetchData(i).then(() => {
+      let cnt = savedData["results"].length;
+      for (let j = 0; j < cnt; j++) {
+          //console.log(savedData["results"][j]);
+          images[i * 5 + j] = ("http://image.tmdb.org/t/p/w500" + savedData["results"][j]["backdrop_path"]);
+          console.log(images[i]);
+      }
+      console.log(i, "___________");
+      let genreDivs = document.querySelectorAll('.genre');
 
-        genreDivs.forEach((genreDiv, index) => {
-            let img = document.createElement('img');
-            img.src = images[index];
-            genreDiv.appendChild(img);
-        });
-    });
+      genreDivs.forEach((genreDiv, index) => {
+          let img = document.createElement('img');
+          img.src = images[index];
+          genreDiv.appendChild(img);
+      });
+  });
 }
